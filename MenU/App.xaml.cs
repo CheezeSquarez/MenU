c6fc2bfa39fbee5bf5e4c68d3a7d136b8d8d3079
CheezeSquarez <mysignups26@gmail.com>
2021-07-08 15:20:27 +0300

    Add project files.

---
+++ MenU/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MenU.Views;
+
+[assembly: ExportFont("BIGSHOULDERSDISPLAY-REGULAR.TTF", Alias = "Big Shoulders")]
+[assembly: ExportFont("Staatliches-Regular.ttf", Alias = "Staatliches")]
+namespace MenU
+{
+    
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SignUp();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
