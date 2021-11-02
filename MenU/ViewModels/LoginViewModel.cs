ed44fc98520f8555e7fb41cca4428ca6715c0448
CheezeSquarez <idosadehrosenfeld@gmail.com>
2021-11-02 20:47:15 +0200

    Added password visibility to login

--- MenU/ViewModels/LoginViewModel.cs
+++ MenU/ViewModels/LoginViewModel.cs
@@ -19,18 +19,32 @@ namespace MenU.ViewModels
         }
 
         #region Attributes
+        private const string OPENEYE = "qr_icon.png";
+        private const string CLOSEDEYE = "default_pfp.jpg";
 
         private string error = "";
         private bool keepLoggedIn = false;
         private string pass = "";
         private MenUWebAPI proxy;
         private string username = "";
+        private string visibilityImage = CLOSEDEYE;
+        private bool visibilityState = true;
 
         #endregion Attributes
 
         #region Properties And Events
 
         public event Action<Page> Push;
+        public string VisibilityImage
+        {
+            get => visibilityImage;
+            set => SetValue(ref visibilityImage, value);
+        }
+        public bool VisibilityState
+        {
+            get => visibilityState;
+            set => SetValue(ref visibilityState, value);
+        }
 
         public string Error
         {
@@ -63,6 +77,19 @@ namespace MenU.ViewModels
         public ICommand ForgotPasswordCommand => new Command(() => { });
         public ICommand LoginCommand => new Command(LoginMethod);
         public ICommand ToSignUpCommand => new Command(() => Push?.Invoke(new SignUp()));
+        public ICommand VisibilityToggle => new Command(() =>
+        {
+            if(visibilityState == false)
+            {
+                VisibilityState = true;
+                VisibilityImage = CLOSEDEYE;
+            }
+            else
+            {
+                VisibilityState = false;
+                VisibilityImage = OPENEYE;
+            }
+        });
 
         private async void LoginMethod()
         {
